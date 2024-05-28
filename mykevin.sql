98c4e92967316431b42dc7d0d69c2f875402f146
dimasramadhan86 <gaspoll1001@gmail.com>
2024-05-28 22:55:28 +0700

    adding sql and env

---
+++ mykevin.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 28, 2024 at 03:53 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mykevin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutme`
+--
+
+CREATE TABLE `aboutme` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `aboutme`
+--
+
+INSERT INTO `aboutme` (`id`, `name`, `desc`, `place`) VALUES
+(1, 'Kevin Saputra', 'I\'m a Computer Science student', 'Sukabumi, Indonesia');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aboutme`
+--
+ALTER TABLE `aboutme`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aboutme`
+--
+ALTER TABLE `aboutme`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
